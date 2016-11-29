b7879cb53d7e766ffac72de5ad8a8255674a438c
HP-2230S-PC\HP-2230S <www.djdanilo@hotmail.com>
2016-11-29 17:52:52 -0500

    contador de palabras

--- lectura.py
+++ lectura.py
@@ -1,25 +1,7 @@
-
-def leertxt():
-    archi=open('bloc.txt','r')
-    linea=archi.readline()
-    while linea!="":
-        cont=0
-        cont=cont+len('bloc.txt')
-        print (linea)
-        linea=archi.readline()
-    print(cont)
-    archi.close()
-
-def leertxttenlista():
-    archi=open('bloc.txt','r')
-    lineas=archi.readlines()
-    print (lineas)
-    archi.close()
-
-leertxt()
-
 def creartxt():
+##archivo vacio a quien se va a llenar
     conP=open('datos2.txt','w')
+##archivo lleno con texto
     archF=open('bloc.txt','r')
     total=0
     linea=archF.readline()
