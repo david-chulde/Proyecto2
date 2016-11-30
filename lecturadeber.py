42396b9c8964216e6cb01daaad7dc0dadc4e0dcd
HP-2230S-PC\HP-2230S <www.djdanilo@hotmail.com>
2016-11-29 19:16:55 -0500

    Contador palabras Harry

--- lecturadeber.py
+++ lecturadeber.py
@@ -1,16 +1,17 @@
+palabra = input("palabra a buscar ?? ")
+archivo = input("Archivo donde Buscar >> ")
 def creartxt():
-##archivo vacio a quien se va a llenar
+##archivo vacio a quien se va a llenar o escribir write
+    repetidas=0
     conP=open('deber2.txt','w')
-##archivo lleno con texto
+##archivo lleno con texto que se va a leer read
     archF=open('harry.txt','r')
-    total=0
-    linea=archF.readline()
-    while linea!='':
-        print(linea)
-        linea=archF.readline()
-        total=len(linea)+total
-        print(total)
-    conP.write(str(total))
-    archF.close()
-    conP.close()
+    lines=archF.readlines()
+    for line in lines:
+        palabras=line.split(' ')
+        for p in palabras:
+            if p==palabra:
+                repetidas=repetidas+1
+    print("la palabra \"{0}\" se repite {1} veces en el Archivo {2}".format(palabra,repetidas,archivo))
+    
 creartxt()
